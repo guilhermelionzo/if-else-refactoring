dcc90ded01b735d5f72cdddf399b933c4f9d20df
Guilherme Silva Lionço <guilherme.lionzo@gmail.com>
2021-02-14 12:37:42 -0300

    add refactored code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//sonar token: 571fd96ddc93d6599add010ac0624e96a221c167
+
+namespace bar
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var recipeBook = new RecipeBook(Console.ReadLine, Console.WriteLine);
+            var bartender = new Bartender(Console.ReadLine, Console.WriteLine, recipeBook);
+
+            while (true)
+                bartender.AskForDrink();
+        }
+    }
+}
---
+++ RecipeBook.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace bar
+{
+    public class RecipeBook
+    {
+        private readonly Dictionary<string, Action> _recipes;
+        private readonly Func<string> _inputProvider;
+        private readonly Action<string> _outputProvider;
+        private bool isBelowEighteen(int age) => age >= 18;
+
+        public RecipeBook(Func<string> inputProvider, Action<string> outputProvider)
+        {
+            _inputProvider = inputProvider;
+            _outputProvider = outputProvider;
+
+            _recipes = new Dictionary<string, Action>
+            {
+                {"beer",ServeBeer},
+                {"juice",ServeJuice},
+            };
+        }
+
+        public void MakeRecipe(string drinkName) => _recipes[drinkName]();
+        public IEnumerable<string> GetAvailableDrinkNames() => _recipes.Keys;
+        private void ServeJuice() => _outputProvider("Here you go! Fresh and nice juice.");
+        private void HandlerInvalidInput() => _outputProvider("Input not valid");
+
+        private void ServeBeer()
+        {
+            _outputProvider("Not so fast. How old are you?");
+
+            if (!int.TryParse(_inputProvider(), out var age))
+            {
+                HandlerInvalidInput();
+                return;
+            }
+
+            HandleBeerAndCheckAge(age);
+        }
+
+        private void HandleBeerAndCheckAge(int age)
+        {
+            if (isBelowEighteen(age))
+            {
+                _outputProvider("Here you go! Cold beer.");
+                return;
+            }
+
+            _outputProvider("Sorry but are not older to order a beer.");
+        }
+    }
+}
